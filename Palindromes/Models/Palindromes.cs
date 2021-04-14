e1a5173f8ab99ed00a60b525ffcebd3863e8eac8
Saoud <saoud@saoud.dev>
2021-04-13 20:31:22 -0700

    added functionality to ignore special chars
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

--- Palindromes/Models/Palindromes.cs
+++ Palindromes/Models/Palindromes.cs
@@ -1,14 +1,15 @@
 using System;
 using System.Linq;
-
+using System.Text.RegularExpressions;
 namespace Palindromes
 {
   public class Palindrome
 	{
+		
         public bool IsPalindrome(string stringToTest)
         {
             string lowerWords = stringToTest.ToLower();
-            string cleanedString = String.Concat(lowerWords.Where(c => !Char.IsWhiteSpace(c)));
+            string cleanedString = RemoveSpecialCharacters(String.Concat(lowerWords.Where(c => !Char.IsWhiteSpace(c))));
             char[] cleanedStringArr = cleanedString.ToCharArray();
             Array.Reverse(cleanedStringArr);
             string reversed = new string(cleanedStringArr);
@@ -21,5 +22,10 @@ namespace Palindromes
                 return false;
             }
         }
+	public static string RemoveSpecialCharacters(string input)
+{
+    Regex r = new Regex("(?:[^a-z0-9 ]|(?<=['\"])s)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+    return r.Replace(input, String.Empty);
+}
 	}
 }
